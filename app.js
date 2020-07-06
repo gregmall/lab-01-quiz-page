1179fc527acb15a1d534d463ab7aead431536e48
Greg Mall <gregmall157@gmail.com>
2020-07-06 15:02:17 -0700

    have the function working

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 // import functions and grab DOM elements
+const myButton= document.getElementById('quizbutton');
+
 
 // initialize state
+myButton.addEventListener('click', () =>{
+    
+    const name = prompt ('What is your name?');
+    console.log(name);
+    const answer1 = prompt('Are cats mammals?');
+    const answer2 = prompt('Do cats like to chase laser lights?');
+    const answer3 = prompt('Are cats pretty much the best pet in the world');
+    console.log(answer1, answer2, answer3);
+})
 
 // set event listeners to update state and DOM
